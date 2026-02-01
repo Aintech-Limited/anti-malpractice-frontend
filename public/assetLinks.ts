1b6346913a0748b31aa69056350d2bf943e164b9
johnson-oragui <johnson.oragui@gmail.com>
2026-02-01 21:23:33 +0100

    feat: Add assets

--- public/assetLinks.ts
+++ public/assetLinks.ts
@@ -5,3 +5,6 @@ export const bookImageTwo =
 	'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=1001';
 export const bookImageThree =
 	'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=1002';
+
+export const studentReadingImage = '/assets/student-reading.png';
+export const heroImage = '/assets/hero-image.png';
