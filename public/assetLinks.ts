c6e6fa6eba74399a92a68490bea51357f68cd5f9
johnson-oragui <johnson.oragui@gmail.com>
2026-02-01 19:09:13 +0100

    feat: Add assets

---
+++ public/assetLinks.ts
@@ -0,0 +1,7 @@
+export const aintechLogo = '/assets/aintechLogo.png';
+export const bookImageOne =
+	'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=1000';
+export const bookImageTwo =
+	'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=1001';
+export const bookImageThree =
+	'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=1002';
