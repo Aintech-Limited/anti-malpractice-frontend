0bdf8c13b3a43d286f4b9d22dd40e1e8390fa2db
johnson-oragui <johnson.oragui@gmail.com>
2026-02-25 12:27:18 +0100

    feat: Update decodeMyJwt util

--- src/lib/serverHelper.ts
+++ src/lib/serverHelper.ts
@@ -1,15 +1,17 @@
 'use server';
 
-import { decodeJwt } from 'jose';
+import { decodeJwt, JWTPayload } from 'jose';
 
 /**
  * Get JWT header without verification
  * @param {string} token - The JWT token
  * @returns {Object} - The decoded header
  */
-export async function decodeMyJwt(token: string) {
+export async function decodeMyJwt(
+	token: string,
+): Promise<JWTPayload & { profileType: string }> {
 	try {
-		return decodeJwt(token);
+		return decodeJwt(token) as JWTPayload & { profileType: string };
 	} catch (error) {
 		throw new Error(`Failed to decode header: ${(error as Error).message}`);
 	}
