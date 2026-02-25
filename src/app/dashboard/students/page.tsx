ee3bd28fc9c67a8fcd9702cbbcf011167fdbbdb2
johnson-oragui <johnson.oragui@gmail.com>
2026-02-25 12:41:57 +0100

    feat: Add exams page

---
+++ src/app/dashboard/students/page.tsx
@@ -0,0 +1,7 @@
+import StudentDashboard from '@/src/components/Dashboard/Student/StudentDashboard';
+
+const StudentDashboardPage = () => {
+	return <StudentDashboard />;
+};
+
+export default StudentDashboardPage;
