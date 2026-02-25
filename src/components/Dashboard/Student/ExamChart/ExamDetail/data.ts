ef4d4c37a06f08677b10e6faf8674c2e7fc7d9fb
johnson-oragui <johnson.oragui@gmail.com>
2026-02-25 12:40:03 +0100

    feat: Add dummay exam data

---
+++ src/components/Dashboard/Student/ExamChart/ExamDetail/data.ts
@@ -0,0 +1,32 @@
+export const stats = [
+	{ title: 'Full Marks', data: { MCQ: '30', Written: '70', Total: '100' } },
+	{
+		title: 'Duration',
+		data: { MCQ: '30m', Written: '2h 40m', Total: '3h 10m' },
+	},
+	{
+		title: 'Total Question',
+		data: { MCQ: '30', Written: '11', Total: '41' },
+	},
+	{
+		title: 'Date and Time',
+		data: {
+			Date: '12 April 2026',
+			Time: '11:00AM - 1:30PM',
+			Result: '21 April 2026',
+		},
+	},
+];
+
+export const guidelines = [
+	'Prepare Your Equipment: Ensure your device and internet connection are ready and reliable.',
+	'Understand the Exam Format: Familiarize yourself with the exam platform and procedures.',
+	'Secure Exam Environment: Choose a quiet, undisturbed, well-lit space for the exam.',
+	'No Unauthorized Resources: Avoid using any resources not explicitly allowed.',
+	'Stay Honest: Maintain academic integrity and avoid any form of cheating.',
+	"Don't Navigate Away: Stay on the test exam page unless otherwise instructed.",
+	'Stick to the Time Limit: Manage your time effectively within the exam duration.',
+	'Backup Your Work: Save your work periodically, if permitted or queries immediately.',
+	'Communicate Difficulties Promptly: Report technical issues or queries immediately.',
+	'Review Before Submission: Double-check all answers before final submission.',
+];
