81cdf9d0d92c15dd1951b0da15152bf8513d469e
johnson-oragui <johnson.oragui@gmail.com>
2026-02-25 12:34:46 +0100

    feat: Add face verify and enroll proxies

---
+++ src/app/api/v1/auth/face-verify/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+	try {
+		const cookieHeader = request.headers.get('cookie');
+		const formData = await request.formData();
+		// console.log([...formData.keys()]);
+
+		if (!formData.has('file')) {
+			return NextResponse.json({ message: 'Missing image' }, { status: 400 });
+		}
+
+		const response = await fetch(
+			`${process.env.BACKEND_API_URL}/v1/auth/face/verify`,
+			{
+				method: 'POST',
+				credentials: 'include',
+				headers: {
+					...(cookieHeader && { cookie: cookieHeader }),
+				},
+				body: formData,
+			},
+		);
+
+		if (!response.ok) {
+			return NextResponse.json(
+				{ ...(await response.json()) },
+				{ status: response.status },
+			);
+		}
+		const data = await response.json();
+
+		return NextResponse.json(data, { status: response.status });
+	} catch (error) {
+		console.error('error face verify: ', error);
+		return NextResponse.json(
+			{ message: 'face verify failed' },
+			{ status: 500 },
+		);
+	}
+}
