ade1a29b384008434f0f497e07e0775e1494d611
johnson-oragui <johnson.oragui@gmail.com>
2026-04-08 20:18:26 +0100

    feat: Add proxy for fetching course-materials by id

---
+++ src/app/api/v1/course-materials/[courseId]/route.ts
@@ -0,0 +1,32 @@
+import { apiProxy } from '@/src/lib/serverHelper';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+	request: NextRequest,
+	{ params }: { params: Promise<{ courseId: string }> },
+) {
+	try {
+		const response = await apiProxy(
+			`${process.env.BACKEND_API_URL}/v1/course-materials/${(await params).courseId}`,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			},
+		);
+
+		const data = await response.json();
+
+		if (!response.ok) {
+			return NextResponse.json(data, { status: response.status });
+		}
+
+		return NextResponse.json(data, { status: response.status });
+	} catch (error) {
+		console.error('Error fetching available materials:', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
