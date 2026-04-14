ba23165ea66d3a2d0bcac7530aa1f6e132bcfb4c
johnson-oragui <johnson.oragui@gmail.com>
2026-04-14 12:59:40 +0100

    feat: Add API router for fetching exam questions

---
+++ src/app/api/v1/exams/questions/[examId]/route.ts
@@ -0,0 +1,35 @@
+import { apiProxy } from '@/src/lib/serverHelper';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+	request: NextRequest,
+	{ params }: { params: Promise<{ examId: string }> },
+) {
+	try {
+		const searchedParams = request.nextUrl.searchParams;
+		const response = await apiProxy(
+			`${process.env.BACKEND_API_URL}/v1/exams/questions/${(await params).examId}?${searchedParams.toString().replace('100', '50')}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			},
+		);
+
+		const data = await response.json();
+
+		if (!response.ok) {
+			return NextResponse.json(data, { status: response.status });
+		}
+		console.log(data);
+
+		return NextResponse.json(data, { status: response.status });
+	} catch (error) {
+		console.error('Error fetching exam questions:', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
