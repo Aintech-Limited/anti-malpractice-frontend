c389a7eed98a520ef862b73c913eb831309fbfea
johnson-oragui <johnson.oragui@gmail.com>
2026-04-15 22:36:15 +0100

    feat: Add CourseItem component

---
+++ src/components/Dashboard/Student/CoursesCatalog/CourseItem/CourseItem.tsx
@@ -0,0 +1,30 @@
+import { getStatusColor } from '@/src/lib/helper';
+import { CourseHeader } from '../CourseHeader/CourseHeader';
+import { ICourseItemProps } from './interface';
+import { CourseDetails } from '../CourseDetails/CourseDetails';
+
+export const CourseItem = ({
+	course,
+	status,
+	isExpanded,
+	isRegistering = false,
+	onToggleDetails,
+	onCourseSelect,
+	onRegister,
+}: ICourseItemProps) => {
+	return (
+		<div className={`p-4 transition-colors ${getStatusColor(status)}`}>
+			<CourseHeader
+				course={course}
+				status={status}
+				isExpanded={isExpanded}
+				isRegistering={isRegistering}
+				onToggleDetails={onToggleDetails}
+				onCourseSelect={onCourseSelect}
+				onRegister={onRegister}
+			/>
+
+			{isExpanded && <CourseDetails course={course} status={status} />}
+		</div>
+	);
+};
