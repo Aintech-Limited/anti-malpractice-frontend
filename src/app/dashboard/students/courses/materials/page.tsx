38f32a5ec2e5ec6ceed70600fea5fea047ff9530
johnson-oragui <johnson.oragui@gmail.com>
2026-04-15 22:38:24 +0100

    feat: Add CourseCatalog page

---
+++ src/app/dashboard/students/courses/materials/page.tsx
@@ -0,0 +1,11 @@
+import CourseMaterialsPage from '@/src/components/Dashboard/Student/CourseMaterial/CourseMaterial';
+import { ICourseMaterialsProps } from '@/src/components/Dashboard/Student/CourseMaterial/interface';
+
+export default async function CourseMaterialPage({
+	searchParams,
+}: ICourseMaterialsProps) {
+	const { limit, page, sort, stats } = await searchParams;
+	return (
+		<CourseMaterialsPage limit={limit} page={page} sort={sort} stats={stats} />
+	);
+}
