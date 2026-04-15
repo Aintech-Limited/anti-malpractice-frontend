23693a08746cfb15ee4b4dffd8145adcb4f61d40
johnson-oragui <johnson.oragui@gmail.com>
2026-04-15 22:43:14 +0100

    feat: Add RegisteredCourses component

---
+++ src/components/Dashboard/Student/RegisteredCourses/interface.ts
@@ -0,0 +1,58 @@
+export interface RegisteredCourse {
+	id: string;
+	courseCode: string;
+	courseId: string;
+	courseTitle: string;
+	courseDescription: string;
+	status: string;
+	department: string;
+	departmentId: string;
+	courseCredits?: number;
+	semester?: number;
+	level?: number;
+	registeredAt: string;
+
+	progress?: number;
+	lastAccessed?: string;
+	MaterialCover?: string;
+	lecturer?: {
+		id?: string;
+		firstName: string;
+		lastName: string;
+	};
+	rating?: number;
+	fileURL?: string;
+	nextClass?: {
+		day: string;
+		time: string;
+		venue: string;
+	};
+	assignments?: {
+		id: string;
+		title: string;
+		dueDate: string;
+		status: 'pending' | 'submitted' | 'graded';
+	}[];
+}
+
+export interface RegisteredCoursesResponse {
+	success: boolean;
+	message: string;
+	meta: RegisteredCoursesResponseMeta;
+	data: RegisteredCourse[];
+}
+
+export interface RegisteredCoursesResponseMeta {
+	totalItems: number;
+	totalPages: number;
+	page: number;
+	limit: number;
+	hasNextPage: boolean;
+	hasPreviousPage: boolean;
+}
+
+export interface RegisteredCoursesClientProps {
+	meta: RegisteredCoursesResponseMeta;
+	courses: RegisteredCourse[];
+	error: string | null;
+}
