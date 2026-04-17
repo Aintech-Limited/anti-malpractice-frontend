085ab26a00f444b5a3b0df252df27c4c679a8bb5
johnson-oragui <johnson.oragui@gmail.com>
2026-04-17 22:35:52 +0100

    feat: Add CheckoutContainer component

---
+++ src/components/Dashboard/Checkout/CheckoutContainer/CheckoutContainer.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+export const CheckoutContainer = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className="min-h-screen flex items-center justify-center bg-gray-50">
+			<div className="max-w-md w-full mx-auto p-8">{children}</div>
+		</div>
+	);
+};
