671d96a5c91044120bb7c969fa5078ce47c3e588
johnson-oragui <johnson.oragui@gmail.com>
2026-04-19 12:01:03 +0100

    feat: Add AddAccountForm interface

---
+++ src/components/Dashboard/Lecturer/Accounts/AddAccountForm/interface.ts
@@ -0,0 +1,13 @@
+import { IBank, IFormData } from '../interface';
+
+export interface IAddAccountFormProps {
+	banks: IBank[];
+	accountsCount: number;
+	maxAccounts: number;
+	loading: boolean;
+	verifyingAccount: boolean;
+	formData: IFormData;
+	onFormChange: (data: Partial<IFormData>) => void;
+	onVerifyAccount: () => Promise<void>;
+	onSubmit: (e: React.FormEvent) => Promise<void>;
+}
