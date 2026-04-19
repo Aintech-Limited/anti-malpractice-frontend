7e7be86302d6fb47cdf6690aa57ce267bb24f0f3
johnson-oragui <johnson.oragui@gmail.com>
2026-04-19 12:28:38 +0100

    feat: Add CourseMaterialPage

---
+++ src/app/dashboard/lecturers/courses/page.tsx
@@ -0,0 +1,5 @@
+import CourseMaterialsPage from '@/src/components/Dashboard/Student/CourseMaterial/CourseMaterial';
+
+export default function CourseMaterialPage() {
+	return <CourseMaterialsPage />;
+}
