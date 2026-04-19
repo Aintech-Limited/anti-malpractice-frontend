a15ca3fa9854ee0c9940ceff95b675d27f9892eb
johnson-oragui <johnson.oragui@gmail.com>
2026-04-19 11:10:41 +0100

    feat: Add student fetch exam reg proxy route

--- src/app/api/v1/exams/students/registration/route.ts
+++ src/app/api/v1/exams/students/registration/route.ts
@@ -1,30 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { apiProxy } from '@/src/lib/serverHelper';
 
-export async function GET(
-	request: NextRequest,
-	params: Promise<{ id: string }>,
-) {
+export async function GET(request: NextRequest) {
 	try {
+		const params = request.nextUrl.searchParams;
 		const response = await apiProxy(
-			`${process.env.BACKEND_API_URL}/v1/departments/${(await params).id}`,
+			`${process.env.BACKEND_API_URL}/v1/exams/students/registration?${params.toString()}`,
 			{
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				method: 'GET',
 			},
 		);
 
 		const data = await response.json();
 
 		if (!response.ok) {
+			console.error('data: ', data);
 			return NextResponse.json(data, { status: response.status });
 		}
 
 		return NextResponse.json(data, { status: response.status });
 	} catch (error) {
-		console.error('Error fetching department stats:', error);
+		console.error('Error fetching students exams for reg.:', error);
 		return NextResponse.json(
 			{ error: 'Internal server error' },
 			{ status: 500 },
