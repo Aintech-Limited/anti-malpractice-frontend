4344952dfbe47e31c1c868663ea46d761e7fbb85
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 13:33:53 +0100

    feat: Update StudentDashboardPage

--- src/app/dashboard/students/page.tsx
+++ src/app/dashboard/students/page.tsx
@@ -1,7 +1,9 @@
 import StudentDashboard from '@/src/components/Dashboard/Student/StudentDashboard';
+import { fetchStudentDashboard } from '@/src/lib/serverHelper';
 
-const StudentDashboardPage = () => {
-	return <StudentDashboard />;
+const StudentDashboardPage = async () => {
+	const initialData = await fetchStudentDashboard();
+	return <StudentDashboard initialData={initialData.data} />;
 };
 
 export default StudentDashboardPage;
