94471cdf22a6c509f9262e8638d2d79f85c302be
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 13:31:14 +0100

    feat: Add ReviewDocumentsModal component

---
+++ src/components/Dashboard/Admin/Lecturers/ReviewDocumentsModal/interface.ts
@@ -0,0 +1,13 @@
+import { TKycStatusEnum } from '@/src/lib/enums';
+import { ILecturer } from '../interface';
+
+export interface IReviewDocumentsModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	lecturer: ILecturer | null;
+	onVerify: (
+		lecturerId: string,
+		type: 'id' | 'selfie',
+		status: TKycStatusEnum,
+	) => Promise<void>;
+}
