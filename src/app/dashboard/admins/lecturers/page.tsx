6972ea5ab511dacdee7fc31e62ccb876f15867fd
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 13:36:42 +0100

    feat: Add LecturersPage

---
+++ src/app/dashboard/admins/lecturers/page.tsx
@@ -0,0 +1,46 @@
+import { ILecturersPageProps } from '@/src/components/Dashboard/Admin/Lecturers/interface';
+import LecturersClient from '@/src/components/Dashboard/Admin/Lecturers/Lectuters';
+import { extractQueryParams } from '@/src/lib/helper';
+import serverAction from '@/src/lib/serverHelper';
+
+export const extractParams = () => {};
+
+export default async function LecturersPage({
+	searchParams,
+}: ILecturersPageProps) {
+	const {
+		page,
+		limit,
+		search,
+		departmentId,
+		isActive,
+		idVerified,
+		selfieVerified,
+	} =
+		await extractQueryParams<ILecturersPageProps['searchParams']>(searchParams);
+
+	const [lecturersData, departments] = await Promise.all([
+		await (
+			await serverAction()
+		).admins.fetchAdminLecturers({
+			page: Number(page),
+			limit: Number(limit),
+			search,
+			departmentId,
+			isActive: isActive === undefined ? undefined : Boolean(isActive),
+			idVerified: idVerified === undefined ? undefined : Boolean(idVerified),
+			selfieVerified:
+				selfieVerified === undefined ? undefined : Boolean(selfieVerified),
+		}),
+		await (await serverAction()).admins.fetchAdminDepartments(),
+	]);
+
+	return (
+		<LecturersClient
+			initialData={lecturersData}
+			initialPage={Number(page ?? '1')}
+			limit={Number(limit ?? '20')}
+			departments={departments.data}
+		/>
+	);
+}
