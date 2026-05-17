6569c9213f3c8359a311464fd137783324883149
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 13:37:00 +0100

    feat: Add DepartmentsPage

---
+++ src/app/dashboard/admins/departments/page.tsx
@@ -0,0 +1,26 @@
+import DepartmentsClient from '@/src/components/Dashboard/Admin/Departments/Departments';
+import { IDepartmentsPageProps } from '@/src/components/Dashboard/Department/interface';
+import serverAction from '@/src/lib/serverHelper';
+import { isNumber } from 'class-validator';
+
+export default async function DepartmentsPage({
+	searchParams,
+}: IDepartmentsPageProps) {
+	const page = (await searchParams).page;
+	const limit = (await searchParams).limit;
+
+	const initialData = await (
+		await serverAction()
+	).admins.fetchAdminDepartments(
+		isNumber(page) ? Number(page) : 1,
+		isNumber(limit) ? Number(limit) : 20,
+	);
+
+	return (
+		<DepartmentsClient
+			initialData={initialData}
+			initialPage={isNumber(page) ? Number(page) : 1}
+			limit={isNumber(limit) ? Number(limit) : 20}
+		/>
+	);
+}
