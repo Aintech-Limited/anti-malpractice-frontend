4baf0f6d57e9273dbde114f4ed611990035a179f
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 12:57:52 +0100

    feat: Add useProctoringSocket

---
+++ src/lib/hooks/interface.ts
@@ -0,0 +1,8 @@
+import { TViolationType } from '../proctoring/interface';
+
+export interface IWSViolationData {
+	type: TViolationType;
+	severity: number;
+	timestamp: number;
+	metadata: Record<string, any>;
+}
