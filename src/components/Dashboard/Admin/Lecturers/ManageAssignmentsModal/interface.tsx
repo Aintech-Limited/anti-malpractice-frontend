418a9de56b6b9e3255fddd2292cdfc590fcc31e6
johnson-oragui <johnson.oragui@gmail.com>
2026-05-17 13:30:50 +0100

    feat: Add ManageAssignmentsModal component

---
+++ src/components/Dashboard/Admin/Lecturers/ManageAssignmentsModal/interface.tsx
@@ -0,0 +1,10 @@
+import { ILecturerAssignment } from '../interface';
+
+export interface IManageAssignmentsModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	lecturerId: string;
+	lecturerName: string;
+	onUnassign?: (assignmentId: string) => Promise<void>;
+	onReassign?: (assignment: ILecturerAssignment) => void;
+}
