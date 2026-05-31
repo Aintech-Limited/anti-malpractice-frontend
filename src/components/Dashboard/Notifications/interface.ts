a5e158eada60cc8a8c5d3868d430463de0d05483
johnson-oragui <johnson.oragui@gmail.com>
2026-05-31 16:45:06 +0100

    feat: Add INotification

---
+++ src/components/Dashboard/Notifications/interface.ts
@@ -0,0 +1,49 @@
+import { TNotificationTypeEnum } from '@/src/lib/enums';
+
+export interface INotification {
+	id: string;
+	title: string;
+	type: TNotificationTypeEnum;
+	createdAt: string;
+	updatedAt: string;
+
+	message: string;
+	userId: string;
+	isRead: boolean;
+
+	metadata?: {
+		examId?: string;
+		courseId?: string;
+		resultId?: string;
+		actionUrl?: string;
+		[key: string]: any;
+	};
+}
+
+export interface INotificationsResponse {
+	success: boolean;
+	message: string;
+	data: {
+		notifications: INotification[];
+		extra: {
+			unreadCount?: number | null;
+		};
+	};
+	meta: {
+		page: number;
+		limit: number;
+		totalItems: number;
+		totalPages: number;
+		hasNextPage: boolean;
+		hasPreviousPage: boolean;
+	};
+}
+
+export interface IWSNotificationData {
+	id: string;
+	title: string;
+	message: string;
+	type: TNotificationTypeEnum;
+	metadata?: INotification['metadata'];
+	createdAt: string;
+}
