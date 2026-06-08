9b3463376d9369d81c644b087e41715ab7f06a4e
johnson-oragui <johnson.oragui@gmail.com>
2026-06-08 22:38:34 +0100

    feat: Add GradeExamClient component

---
+++ src/app/dashboard/lecturers/grade-exams/[examId]/page.tsx
@@ -0,0 +1,29 @@
+import GradeExamClient from '@/src/components/Dashboard/Lecturer/GradeExam/GradeExam';
+import { IGradeExamPageProps } from '@/src/components/Dashboard/Lecturer/GradeExam/interface';
+import { ProtectedRouteEnum } from '@/src/lib/enums';
+import { getUngradedQuestions } from '@/src/lib/serverHelper';
+import { isUUID } from 'class-validator';
+import { redirect } from 'next/navigation';
+
+export default async function GradeExamPage({
+	params,
+	searchParams,
+}: IGradeExamPageProps) {
+	const { examId } = await params;
+	const { limit, page } = await searchParams;
+	if (!isUUID(examId ?? '')) redirect(ProtectedRouteEnum.LECTURERS);
+
+	const initialQuestions = await getUngradedQuestions(
+		examId,
+		parseInt(page ?? '1'),
+		parseInt(limit ?? '100'),
+	);
+
+	return (
+		<GradeExamClient
+			examId={examId}
+			examTitle={initialQuestions.data.examTitle}
+			initialQuestionsData={initialQuestions}
+		/>
+	);
+}
