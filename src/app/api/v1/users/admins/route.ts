e25f571a016b08873335fe264589df858db979a8
johnson-oragui <johnson.oragui@gmail.com>
2026-07-07 22:55:58 +0100

    feat: Add get admins users route

--- src/app/api/v1/users/admins/route.ts
+++ src/app/api/v1/users/admins/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
 	try {
 		const response = await apiProxy(
-			`${process.env.BACKEND_API_URL}/v1/users/me`,
+			`${process.env.BACKEND_API_URL}/v1/users/admins`,
 			{
 				method: 'GET',
 				credentials: 'include',
@@ -21,9 +21,9 @@ export async function GET(request: NextRequest) {
 
 		return NextResponse.json(data, { status: response.status });
 	} catch (error) {
-		console.error('error get user: ', error);
+		console.error('error getting admin users: ', error);
 		return NextResponse.json(
-			{ message: 'Internal server error', success: false },
+			{ message: 'Internal server error', success: false, data: [] },
 			{ status: 500 },
 		);
 	}
