1e25303171141c2c113e623921d12a8eba9598f8
johnson-oragui <johnson.oragui@gmail.com>
2026-07-21 18:11:11 +0100

    feat: Remove unused props

--- src/components/Dashboard/Student/Result/ExamResults.tsx
+++ src/components/Dashboard/Student/Result/ExamResults.tsx
@@ -1,35 +1,31 @@
-"use client";
+'use client';
 
-import { UserRoleTypeEnum } from "@/src/lib/enums";
-import { IExamResultsProps } from "./interface";
-import StudentExamResults from "./StudentExamResults";
-import StaffExamResults from "./StaffExamResults";
-import { useAuth } from "@/src/providers/auth/AuthContext";
+import { UserRoleTypeEnum } from '@/src/lib/enums';
+import { IExamResultsProps } from './interface';
+import StudentExamResults from './StudentExamResults/StudentExamResults';
+import StaffExamResults from './StaffExamResults/StaffExamResults';
+import { useAuth } from '@/src/providers/auth/AuthContext';
 
 export default function ExamResults({
-  departments = [],
-  exams = [],
-  initialPage = 1,
-  initialLimit = 50,
+	initialPage = 1,
+	initialLimit = 50,
 }: IExamResultsProps) {
-  const { getUserRole } = useAuth();
-  const role = getUserRole()!;
-  if (role === UserRoleTypeEnum.USER) {
-    return (
-      <StudentExamResults
-        initialPage={initialPage}
-        initialLimit={initialLimit}
-      />
-    );
-  }
+	const { getUserRole } = useAuth();
+	const role = getUserRole()!;
+	if (role === UserRoleTypeEnum.USER) {
+		return (
+			<StudentExamResults
+				initialPage={initialPage}
+				initialLimit={initialLimit}
+			/>
+		);
+	}
 
-  return (
-    <StaffExamResults
-      role={role}
-      departments={departments}
-      exams={exams}
-      initialPage={initialPage}
-      initialLimit={initialLimit}
-    />
-  );
+	return (
+		<StaffExamResults
+			role={role}
+			initialPage={initialPage}
+			initialLimit={initialLimit}
+		/>
+	);
 }
