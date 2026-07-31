7b153be294d544e787f286d7d3e08d0bbd5c807d
johnson-oragui <johnson.oragui@gmail.com>
2026-07-31 16:25:53 +0100

    feat: Add LecturersDashboardLayout

--- src/app/dashboard/students/page.tsx
+++ src/app/dashboard/students/page.tsx
@@ -1,9 +1,9 @@
-import StudentDashboard from "@/src/components/Dashboard/Student/StudentDashboard";
-import { fetchStudentDashboard } from "@/src/lib/serverHelper";
+import StudentDashboard from '@/src/components/Dashboard/Student/StudentDashboard';
+import { fetchStudentDashboard } from '@/src/lib/serverHelper';
 
 const StudentDashboardPage = async () => {
-  const initialData = await fetchStudentDashboard();
-  return <StudentDashboard initialData={initialData.data} />;
+	const initialData = await fetchStudentDashboard();
+	return <StudentDashboard initialData={initialData.data} />;
 };
 
 export default StudentDashboardPage;
