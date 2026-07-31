0fd0baa8b694ec70cd2abbd8d3f5a45152c52b9b
johnson-oragui <johnson.oragui@gmail.com>
2026-07-31 16:39:53 +0100

    feat: Add institutionId, departmentId, country, state to signup payload

--- src/components/SignUp/SignupProfileSelector/data.ts
+++ src/components/SignUp/SignupProfileSelector/data.ts
@@ -1,24 +1,24 @@
-import { ProfileTypeEnum } from "@/src/lib/enums";
-import { IChooseProfileData } from "./interface";
-import { APP_NAME } from "@/src/lib/data";
+import { UserRoleTypeEnum } from '@/src/lib/enums';
+import { IChooseProfileData } from './interface';
+import { APP_NAME } from '@/src/lib/data';
 
 export const profilesTypes: IChooseProfileData[] = [
-  {
-    id: 1,
-    icon: "Sun",
-    text: `I'm a lecturer, i want to sell my product/service on ${APP_NAME}`,
-    type: ProfileTypeEnum.LECTURER,
-  },
-  {
-    id: 2,
-    icon: "Users",
-    text: "I am a Student, I want ...",
-    type: ProfileTypeEnum.STUDENT,
-  },
-  {
-    id: 3,
-    icon: "Handshake",
-    text: "I am an Author, I want to...",
-    type: ProfileTypeEnum.VENDOR,
-  },
+	{
+		id: 1,
+		icon: 'Sun',
+		text: `I'm a lecturer, i want to sell my product/service on ${APP_NAME}`,
+		type: UserRoleTypeEnum.LECTURER,
+	},
+	{
+		id: 2,
+		icon: 'Users',
+		text: 'I am a Student, I want ...',
+		type: UserRoleTypeEnum.STUDENT,
+	},
+	{
+		id: 3,
+		icon: 'Handshake',
+		text: 'I am an Author, I want to...',
+		type: UserRoleTypeEnum.VENDOR,
+	},
 ];
