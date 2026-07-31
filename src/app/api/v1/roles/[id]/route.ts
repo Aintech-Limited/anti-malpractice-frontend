a8a9d0402296a76b883850c738ea10de178af6da
johnson-oragui <johnson.oragui@gmail.com>
2026-07-31 16:40:39 +0100

    feat: Add roles proxy routers

---
+++ src/app/api/v1/roles/[id]/route.ts
@@ -0,0 +1,68 @@
+import { apiProxy } from '@/src/lib/serverHelper';
+import { NextRequest, NextResponse } from 'next/server';
+
+const BACKEND_API_URL = process.env.BACKEND_API_URL;
+
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: Promise<{ id: string }> },
+) {
+	try {
+		const { id } = await params;
+
+		const response = await apiProxy(`${BACKEND_API_URL}/v1/roles/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			const error = await response.json();
+			console.error('error: ', JSON.stringify(error));
+			return NextResponse.json(error, { status: response.status });
+		}
+
+		const data = await response.json();
+		return NextResponse.json(data, { status: response.status });
+	} catch (error) {
+		console.error('Failed to delete role :', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function PATCH(
+	request: NextRequest,
+	{ params }: { params: Promise<{ id: string }> },
+) {
+	try {
+		const { id } = await params;
+		const body = await request.json();
+
+		const response = await apiProxy(`${BACKEND_API_URL}/v1/roles/${id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body),
+		});
+
+		if (!response.ok) {
+			const error = await response.json();
+			console.error('error: ', JSON.stringify(error));
+			return NextResponse.json(error, { status: response.status });
+		}
+
+		const data = await response.json();
+		return NextResponse.json(data, { status: response.status });
+	} catch (error) {
+		console.error('Failed to delete role :', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
