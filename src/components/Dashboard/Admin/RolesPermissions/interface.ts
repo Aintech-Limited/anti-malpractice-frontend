5c62b1c35d4a36e4c34312044a6222f07a75b448
johnson-oragui <johnson.oragui@gmail.com>
2026-07-31 16:34:17 +0100

    feat: Add IRolePermission

---
+++ src/components/Dashboard/Admin/RolesPermissions/interface.ts
@@ -0,0 +1,68 @@
+export interface IPermission {
+	id: string;
+	name: string;
+	description: string;
+}
+
+export interface IRolePermission {
+	permission: {
+		id: string;
+		name: string;
+		description: string;
+		module: string;
+	};
+}
+
+export interface IRole {
+	id: string;
+	name: string;
+	description: string;
+	createdAt: string;
+	rolePermissions?: IRolePermission[];
+}
+
+export interface IGetRoleResponse {
+	data: IRole[];
+	meta: IMeta;
+	message: string;
+	success: boolean;
+}
+
+export interface IGroupedPermissions {
+	[module: string]: IPermission[];
+}
+
+export interface IGetGroupedPermissionResponse {
+	data: IGroupedPermissions;
+	message: string;
+	success: boolean;
+}
+
+export interface IMeta {
+	totalItems: number;
+	page: number;
+	limit: number;
+	totalPages: number;
+	hasNextPage: boolean;
+	hasPreviousPage: boolean;
+}
+
+export interface IRolesPageClientProps {
+	initialRoles: IRole[];
+	initialMeta: IMeta;
+	groupedPermissions: IGroupedPermissions;
+}
+
+export interface ISearchParams {
+	page?: string;
+	limit?: string;
+	search?: string;
+	includePermissions?: string;
+}
+
+export interface IValidatedParams {
+	page: number;
+	limit: number;
+	search?: string;
+	includePermissions: boolean;
+}
