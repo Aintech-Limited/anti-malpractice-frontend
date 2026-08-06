e11fa97dd13a980980fee353e5174d23e94697b4
johnson-oragui <johnson.oragui@gmail.com>
2026-08-06 16:01:33 +0100

    feat: Add VendorBooksPage

---
+++ src/app/dashboard/vendors/books/page.tsx
@@ -0,0 +1,32 @@
+import { ICourseMaterialsPageProps } from '@/src/components/Dashboard/Lecturer/CourseMaterials/interface';
+import BooksClient from '@/src/components/Dashboard/Vendors/Books/Books';
+import { normaliseCourseMaterialsParams } from '@/src/lib/helper';
+import { getInitialVendorBooks } from '@/src/lib/serverHelper';
+
+export default async function VendorBooksPage({
+	searchParams,
+}: ICourseMaterialsPageProps) {
+	const params = await searchParams;
+	const result = await getInitialVendorBooks(
+		normaliseCourseMaterialsParams(params) as any,
+	);
+	return (
+		<BooksClient
+			initialBooks={{
+				books: result.data?.materials,
+				totalRevenue: result.data?.totalRevenue,
+			}}
+			initialMeta={result.meta}
+			initialFilters={{
+				...params,
+				page: Number(params.page ?? '1'),
+				limit: Number(params.limit ?? '10'),
+				sortBy: params?.sortBy ?? 'createdAt',
+				sortOrder: params.sortOrder ?? 'DESC',
+				fileType: params.fileType ?? '',
+				isFree: params?.isFree ?? '',
+				search: params.search ?? '',
+			}}
+		/>
+	);
+}
