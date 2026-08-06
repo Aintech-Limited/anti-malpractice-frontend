cd4c6b404c44f8a0d033b318b02ab4189e3e24b7
johnson-oragui <johnson.oragui@gmail.com>
2026-08-06 16:02:00 +0100

    feat: Add VendorPaymentsViewPage

---
+++ src/app/dashboard/vendors/finance/page.tsx
@@ -0,0 +1,25 @@
+import { IPaymentsPageProps } from '@/src/components/Dashboard/Student/Payments/interface';
+import { Suspense } from 'react';
+import { fetchPayments } from '@/src/lib/serverHelper';
+import PaymentsSkeleton from '@/src/components/Dashboard/Student/Payments/PaymentsSkeleton/PaymentsSkeleton';
+import Payment from '@/src/components/Dashboard/Student/Payments/Payment';
+
+export default async function VendorPaymentsViewPage({
+	searchParams,
+}: IPaymentsPageProps) {
+	const data = await fetchPayments(searchParams);
+
+	return (
+		<Suspense fallback={<PaymentsSkeleton />}>
+			<Payment
+				initialData={data}
+				initialFilters={{
+					type: (await searchParams).type || '',
+					sortBy: (await searchParams).sortBy || 'createdAt',
+					page: parseInt((await searchParams).page || '1'),
+					limit: parseInt((await searchParams).limit || '50'),
+				}}
+			/>
+		</Suspense>
+	);
+}
