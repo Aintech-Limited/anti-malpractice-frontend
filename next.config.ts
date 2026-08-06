37aad9ccc7ea852e281fa47945a4a81d4efc9150
johnson-oragui <johnson.oragui@gmail.com>
2026-08-06 15:56:37 +0100

    feat: Add standalone build script

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-icons-png.flaticon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
+	/* config options here */
+	output: 'standalone',
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'www.gstatic.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn-icons-png.flaticon.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+			},
+		],
+	},
 };
 
 export default nextConfig;
