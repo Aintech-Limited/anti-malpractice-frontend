7864e945720c8e6f714bc4b2b7405e9abee4b910
johnson-oragui <johnson.oragui@gmail.com>
2026-08-19 23:44:45 +0100

    feat: Add IVendorsDashboardProps

---
+++ src/components/Dashboard/Vendors/Dashboard/interface.ts
@@ -0,0 +1,30 @@
+export type TMonth =
+	| 'Jan'
+	| 'Feb'
+	| 'Mar'
+	| 'Apr'
+	| 'May'
+	| 'Jun'
+	| 'July'
+	| 'Aug'
+	| 'Sept'
+	| 'Nov'
+	| 'Dec';
+export interface IVendorsDashboardProps {
+	initialData: {
+		message: string;
+		success: boolean;
+		data: {
+			stats: { booksAdded: number; booksApproved: number; booksSold: number };
+			barchart: { label: TMonth; value: number }[];
+			lastestPurchase: {
+				id: string;
+				student?: { firstName: string; lastName: string };
+				amount: string;
+				currency: string;
+				createdAt: string;
+				updatedAt: string;
+			}[]; // 5 is limit for this
+		};
+	};
+}
